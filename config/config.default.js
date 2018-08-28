71ee8a6dd91a9f10420ff7934be70eec12166b6e
田亚楠 <hztianyanan@HIH-L-3827.local>
2018-08-28 14:52:59 +0800

    egg-init --type=plugin

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * egg-multiend default config
+ * @member Config#multiend
+ * @property {String} SOME_KEY - some description
+ */
+exports.multiend = {
+  pc: 'pc',
+  wap: 'wap',
+  api: 'api',
+  page: 'page',
+};
